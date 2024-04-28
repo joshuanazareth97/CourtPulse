4c8448662379736b3dbbe17499735601e67395cc
Joshua <joshuanazareth97@gmail.com>
2024-04-28 15:08:22 +0530

    Add bots list

---
+++ landing-page/constants/bots.ts
@@ -0,0 +1,13 @@
+import supremeCourtImage from "../public/img/court-logos/supreme-court.png";
+
+export const bots = [
+  {
+    name: "Supreme Court",
+    link: "https://www.supremecourt.gov/",
+    image: supremeCourtImage,
+  },
+  {
+    name: "Delhi High Court",
+    link: "https://www.supremecourt.gov/",
+  },
+];
