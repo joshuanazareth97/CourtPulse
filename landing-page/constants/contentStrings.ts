3321a746907ffd4f35cb4a0619fb9280d22abd30
Joshua <joshuanazareth97@gmail.com>
2024-04-28 17:08:26 +0530

    Fix github link

--- landing-page/constants/contentStrings.ts
+++ landing-page/constants/contentStrings.ts
@@ -1,4 +1,4 @@
 export const CALL_TO_ACTION = "Get started on Telegram";
 export const CTA_LINK = "https://t.me/your_bot_name";
 
-export const GITHUB_LINK = "https://github.com/joshuanazareth97/court-pulse";
+export const GITHUB_LINK = "https://github.com/joshuanazareth97/CourtPulse";
