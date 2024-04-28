3e3eb7834a880fd57a66b79183eaf77079bfa10f
Joshua <joshuanazareth97@gmail.com>
2024-04-28 13:34:40 +0530

    Update colors

--- landing-page/tailwind.config.js
+++ landing-page/tailwind.config.js
@@ -3,12 +3,16 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         trueGray: colors.neutral,
+        midnight: "#00203e",
       },
     },
     fontFamily: {
