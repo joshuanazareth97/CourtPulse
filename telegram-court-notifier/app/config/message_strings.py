8b1936263db360126c332ca8a3aa9f7bb7aa1c04
Joshua <joshuanazareth97@gmail.com>
2024-05-02 09:09:49 +0530

    Update welcome message with dynamic params

--- telegram-court-notifier/app/config/message_strings.py
+++ telegram-court-notifier/app/config/message_strings.py
@@ -1,7 +1,6 @@
 WELCOME_MESSAGE = (
-    "Hello! I am the Case Monitoring Bot. \n"
+    "Hello! I am the Case Monitoring Bot for %s. \n"
     "You can use me to get notifications about the status of court cases. \n\n"
-
     "Use /monitor <Court Number> <Case Number> to start monitoring a case. \n"
     # "Use /status to see all the cases you are currently monitoring. \n"
     # "Use /clear to stop monitoring all cases."
