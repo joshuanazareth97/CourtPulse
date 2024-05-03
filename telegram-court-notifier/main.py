99b3e2cb8586f0950ba8785f3f61ed4fbcabe3c7
Joshua <joshuanazareth97@gmail.com>
2024-05-03 16:35:48 +0530

    Add DelhiHighCourtBot and update main.py

--- telegram-court-notifier/main.py
+++ telegram-court-notifier/main.py
@@ -1,15 +1,35 @@
+import os
 from telegram.ext import Application, CommandHandler
-from app.bots import SupremeCourtBot
+from app.bots import SupremeCourtBot, DelhiHighCourtBot
+from app.config import settings
+from app.core import BaseBot
+from app.core.utils import print_dict_as_table
+
+# Add new bots here and set the key in the BOT_NAME env var
+BOT_MAP: dict[str, BaseBot] = {
+    "SupremeCourt": SupremeCourtBot,
+    "DelhiHighCourt": DelhiHighCourtBot,
+}
 
 
 def main():
     # TODO: Find the recced way of exiting a script like this
     """Run bot."""
-    bot = SupremeCourtBot()
-    print("Telegram Case Listing Bot is running...")
+    bot_name = settings.BOT_NAME
+    Bot = BOT_MAP.get(bot_name)
+    if not bot_name or not Bot:
+        raise ValueError(
+            f"Valid BOT_NAME env var is required, you provided: {bot_name}"
+        )
+    bot: BaseBot = Bo()
+    print(f"Telegram Case Listing Bot ({bot.display_name}) is running...")
+    print("Config:")
+    print_dict_as_table(settings.__dict__)
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(bot.bot_token).build()
-    application.job_queue.run_repeating(bot.check_for_cases, interval=30)
+    application.job_queue.run_repeating(
+        bot.check_for_cases, interval=bot.polling_interval
+    )
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler(["start", "help"], bot.start))
     application.add_handler(
